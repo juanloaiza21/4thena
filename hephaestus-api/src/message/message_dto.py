4c1915754d110d3c39647dc242f52d27f147a058
juloaizar <juan400reyesloaiza@gmail.com>
2025-12-14 11:01:14 -0500

    :bug: fixed bug

--- hephaestus-api/src/message/message_dto.py
+++ hephaestus-api/src/message/message_dto.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
+from typing import Any
+
+class CreateMessageDto(BaseModel):
+    source: str
+    txt: str
+    ratified: bool = False
+    content: dict[str, Any]
 
 
 class RatifyMessageDto(BaseModel):
     message_id: str
+    merchant_id: str
--- hephaestus-api/src/message/message_model.py
+++ hephaestus-api/src/message/message_model.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from pydantic import BaseModel
-from typing import Dict, Any, Optional
+from typing import Dict, Any
 
 
 class MessageSource(str, Enum):
@@ -12,5 +12,6 @@ class MessageSource(str, Enum):
 
 class Message(BaseModel):
     source: MessageSource
-    content: Dict[str, Any]
+    txt: Dict[str, Any]
+    merchant_id: str
     ratified: bool = False
