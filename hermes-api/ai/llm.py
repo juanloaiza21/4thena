8a6429eae2c76f43439cd718d4180d722af22683
Daniel Diaz <danieldiazgonza52@gmail.com>
2025-12-13 20:11:52 -0500

    :sparkies: Added copied services

---
+++ hermes-api/ai/llm.py
@@ -0,0 +1,33 @@
+from typing import List
+import os
+from google import genai
+from dotenv import load_dotenv
+from typing import Optional, Any
+
+load_dotenv()
+class LLMinteractor:
+    def __init__(self, api_key: str | None = None):
+        """
+        Initializes the LLMinteractor with an explicit API key.
+        Fails fast if the API key is missing.
+        """
+        self.api_key = api_key or os.getenv("GEMINI_API_KEY")
+        if not self.api_key:
+            raise ValueError("GEMINI_API_KEY not found in .env file")
+
+        self.client = genai.Client(api_key=self.api_key)
+        self.model_name = 'gemini-2.5-flash'
+
+    def generate(self, prompt : List[dict], model: Optional[str] = None, **kwargs : Any) -> str:
+        try:
+            llm_response = self.client.models.generate_content(
+                model= model or self.model_name,
+                contents=prompt,
+                **kwargs
+            )
+            if llm_response.text is None:
+                return ""
+            return llm_response.text
+        except Exception as e:
+            # print(e)
+            return "An error occurred while processing your request with the LLM."
---
+++ hermes-api/service/summarizer.py
@@ -0,0 +1,22 @@
+from typing import List
+from ai.llm import LLMinteractor
+from ai.embeddings import EmbeddingsService
+from ai.prompts import SUMMARY_PROMPT
+
+class Summarizer:
+    def __init__(self, llm: LLMinteractor):
+        self.llm = llm
+
+    def summarize(self, content: List[dict] | str) -> str:
+
+        send_content = [{"role": "model", "parts": [{"text": SUMMARY_PROMPT}]}]
+
+        if isinstance(content, str):
+            send_content += [{"role": "user", "parts": [{"text": content}]}]
+        else:
+            send_content += content
+
+        return self.llm.generate(send_content)
+    
+    def summarizeEmbedding(self, content: List[dict] | str) -> List[float]:
+        return self.embeddings_service.createEmbedding(self.summarize(content))
